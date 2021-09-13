227db61d5ccee81f29dcec608ac98e6fac5aa9a3
DavidMedin <davidmedin03@gmail.com>
2021-09-13 14:15:52 -0500

    Creating example thing.

--- source.c
+++ source.c
@@ -3,100 +3,101 @@
 #include <string.h>
 
 #include "ecs.h"
-//TODO: another thing to do is create unit tests. IDK how but it is perhaps a good idea
-/*
-int humanID;
+//TODO: Add HasEntity function
+//TODO: Add multi component system calls
+//TODO: think about events
+
+int meatID,humanID,itemID;
 typedef struct{
-	int health;
-	float height;
+    int health;//max 100
+}MeatBag;
+typedef struct{
+    Entity hands[2];//can hold Items
     char* name;
-}Human;
-
-void HumanInit(void* component){
-	Human* hum= component;
-	hum->health = 100;
-	hum->height = 5.0f + 9.0f/12.0f;
-    hum->name = "Joe";
-	printf("Initialized the Human!\n");
-}
-void HumanUpdate(int entityID){
-	Human* human = GetComponent(humanID,entityID);
-    if(!human){printf("Human update failed!\n");return;}
-	printf("%s is %.2f meters tall\n",human->name,human->height);
-}
-
-int talkID;
+}Humanoid;
 typedef struct{
-    char interactKey;
-}Talkable;
-
-void TalkableInit(void* component){
-    Talkable* talk = component;
-    talk->interactKey = 'e';
-}
-void InteractSystem(int eID){
-    Talkable* talk = GetComponent(talkID,eID);
-    printf("did you hit the %c key?\n",talk->interactKey);
-}
-*/
-
-int inventoryID,itemID;
-typedef struct{
-    Entity item;
-}Inventory;
-typedef struct{
-    char* name;
+    int damage;//how much damage when waking somebody/thing
 }Item;
-void InventoryInit(void* rawInv){
-    Inventory* inv = rawInv;
-    inv->item = 0;
+
+void MeatBagInit(void* rawMeat){
+    MeatBag* you = rawMeat;
+    you->health = 100;
 }
-void PrintInventory(int entity){
-    Inventory* inv = GetComponent(inventoryID,entity);
-    if(inv->item != 0) {
-        Item *containedItem = GetComponent(itemID, inv->item);
-        if (containedItem != NULL) {
-            printf("This inventory contains a %s!\n", containedItem->name);
-        } else {
-            inv->item = 0;//NULL
-        }
-    }
+void HumanoidInit(void* human){
+    Humanoid* oid = human;
+    oid->hands[0] = 0;//empty left hand
+    oid->hands[1] = 0;//empty right hand
+    oid->name = "a human";
 }
 void ItemInit(void* rawItem){
     Item* item = rawItem;
-    item->name = "Sword";//default value
+    item->damage = 20;
+}
+void DealDamage(Entity defender,int damage){
+    MeatBag* meat = GetComponent(meatID,defender);
+    if(meat != NULL){
+        meat->health -= damage;
+    }else{
+        printf("defender doesn't have a MeatBag component!\n");
+        return;
+    }
+}
+void Attack(Entity attacker,int hand,Entity defender){
+    //attacker is attacking defender with their {hand} hand!
+    if(hand < 0 || hand > 1){
+        printf("hand index out of range! %d \n",hand);
+        return;
+    }
+    //Get Humanoid Component
+    Humanoid* attackHuman = GetComponent(humanID,attacker);
+    Humanoid* defenderHuman = GetComponent(humanID,defender);
+    if(attackHuman != NULL && defenderHuman != NULL){
+        Item* wackItem = GetComponent(itemID,attackHuman->hands[hand]);
+        if(wackItem != 0){
+            //there is an item in hand! Wack time
+            MeatBag* defenderMeat = GetComponent(meatID,defender);
+            if(defenderMeat != NULL){
+                DealDamage(defender,wackItem->damage);
+                printf("%s delt %d to %s and now %s has %d health!\n",attackHuman->name,wackItem->damage,
+                       defenderHuman->name,defenderHuman->name,defenderMeat->health);
+            }else{
+                printf("defender doesn't have a meatbag component!\n");
+                return;
+            }
+        }else{
+            printf("Item doesn't have the Item component!\n");
+        }
+    }else{
+        printf("Either attacker or defender doesn't have a Humanoid component!\n");
+    }
 }
 
-void PrintPoolStuff(){
-    printf("---------------\n");
-    For_Each(components,iter){
-        Component* comp = Iter_Val(iter,Component);
-        unsigned int packedCount = comp->data.packed.list.count;
-        unsigned int sparseCount = comp->data.sparse.list.count;
-        printf("component %d :\n\tpacked count: %d\n",iter.i,packedCount);
-        printf("\tsparse count: %d\n",sparseCount);
-    }
-    printf("deleted count : %d\n",deleted.list.count);
-    printf("versions count : %d\n",versions.list.count);
-    printf("--------------\n");
-}
 int main(int argc,char** argv){
     setbuf(stdout,0);//bruh why do I have to do this?
     ECSStartup();
-    inventoryID = RegisterComponent(sizeof(inventoryID),InventoryInit);
+
+    humanID = RegisterComponent(sizeof(Humanoid),HumanoidInit);
+    meatID = RegisterComponent(sizeof(MeatBag),MeatBagInit);
     itemID = RegisterComponent(sizeof(Item),ItemInit);
 
-    Entity item = CreateEntity();
-    Entity inv = CreateEntity();
-    AddComponent(item, itemID);
-    AddComponent(inv,inventoryID);
+    //create entities
 
-    ((Inventory*)GetComponent(inventoryID,inv))->item = item;
-    ((Item*) GetComponent(itemID,item))->name = "potion (++str)";
+    Entity sword = CreateEntity();
+    AddComponent(sword,itemID);
+    ((Item*) GetComponent(itemID,sword))->damage = 21;
 
-    CallSystem(PrintInventory,inventoryID);
-    DestroyEntity(item);
-    CallSystem(PrintInventory,inventoryID);
+    Entity orc = CreateEntity();
+    AddComponent(orc,humanID);
+    AddComponent(orc,meatID);
+    ((Humanoid*) GetComponent(humanID,orc))->name = "the orc";
+
+    Entity human = CreateEntity();
+    AddComponent(human,humanID);AddComponent(human,meatID);
+    Humanoid* humanHuman = GetComponent(humanID,human);
+    humanHuman->name = "Jimmy";
+    humanHuman->hands[1] = sword;
+
+    Attack(human,1,orc);
 /*
  *  While -Game loop
  *      get input
@@ -104,5 +105,6 @@ int main(int argc,char** argv){
  *      call system based on what it is
  *          EX: Player attacks Orc. Call Players attack function -> calls Orc's damage function
  */
+
     return 0;
 }
